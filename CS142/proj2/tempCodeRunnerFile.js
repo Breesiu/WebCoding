362a32b05c73f22b5cba03f54039c5d29723ef54
Breezesiu <943975428abc@gmail.com>
2022-07-05 20:03:02 +0800

    Enter

---
+++ CS142/proj2/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+e = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
+// var dateTemplate = new Cs142TemplateProcessor(template);
+
+// var dictionary = {month: 'July', day: '1', year: '2016'};
+// var str = dateTemplate.fillIn(dictionary);
+// console.log(str);
+// assert(str === 'My favorite month is July but not the day 1 or the year 2016');
+
+// //Case: property doesn't exist in dictionary
+// var dictionary2 = {day: '1', year: '2016'};
+// var str = dateTemplate.fillIn(dictionary2);
+// console.log(str);
+
+// assert(str === 'My favorite month is  but not the day 1 or the year 2016');
